a7d0ee22985e7e3836afb2826b22e844c902eed0
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-10 18:35:44 +0100

    Update ReoveryShow.m

--- GPSP/solver/funcs/ReoveryShow.m
+++ GPSP/solver/funcs/ReoveryShow.m
@@ -1,6 +1,6 @@
-function ReoveryShow(xo,x,pos,ind)
-
+function ReoveryShow(xo,x,pos)
     figure('Renderer', 'painters', 'Position', pos)
+    axes('Position', [0.05 0.1 0.9 0.8] );
     stem(find(xo),xo(xo~=0),'bo-','MarkerSize',6, 'LineWidth',1),hold on
     stem(find(x),x(x~=0),'r*:', 'MarkerSize',4, 'LineWidth',1),hold on
     grid on, ymin= -0.1; ymax=0.2;
@@ -8,17 +8,11 @@ function ReoveryShow(xo,x,pos,ind)
     if nnz(xx<0)>0, ymin= min(xx(xx<0))-0.1; end
     if nnz(xx>0)>0, ymax= max(xx(xx>0))+0.1; end   
     axis([1 length(x)  ymin  ymax])
-    if ind
-       snr   = -10*log10(norm(x-xo)^2);
-       st1   = strcat('SNR=',num2str(snr,4));
-       wrong = nnz(xo)-nnz(find(x~=0 & xo~=0)); 
-       st2   = strcat(', s_*=',num2str(nnz(xo)),...
-            ', Number of mis-supports =',num2str(wrong)); 
-%        st2   = strcat(', s_*=',num2str(nnz(xo)),', s=',num2str(nnz(x)));         
-       title(strcat(st1,st2))
-       set(0,'DefaultAxesTitleFontWeight','normal');
-
-       legend('Ground-Truth', 'Recovered', 'Location', 'best')
-    end
+    st1   = strcat('SNR=',num2str(-10*log10(norm(x-xo)^2),4));
+    wrong = nnz(xo)-nnz(find(x~=0 & xo~=0)); 
+    st2   = strcat(', s_*=',num2str(nnz(xo)),...
+            ', Number of mis-supports =',num2str(wrong));         
+    title(strcat(st1,st2))
+    set(0,'DefaultAxesTitleFontWeight','normal');
+    legend('Ground-Truth', 'Recovered')
 end
-
