a99a0e49d4f6f0e6dc09976a05e6a7a1cd7545aa
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-10 18:37:02 +0100

    Update demon_one_run.m

--- GPSP/demon_one_run.m
+++ GPSP/demon_one_run.m
@@ -1,6 +1,6 @@
 clc; close all; clear; warning off
 
-n          = 1000; 
+n          = 2000; 
 m          = ceil(0.5*n);
 s          = ceil(0.01*n);  % sparsity level
 r          = 0.01;          % flipping ratio
@@ -10,16 +10,8 @@ type       = 'Ind';         % 'Ind' or 'Cor'
 k          = ceil(0.01*m);
 out        = GPSP(A,c,s,k);
 x          = out.x; 
-
 fprintf('Time: %6.3f\n',out.time);
 fprintf('SNR:  %6.3f\n',-10*log10(norm(x-xo)^2));
 fprintf('HD:   %6.3f\n',nnz(sign(A*x)-c)/m)
 fprintf('HE:   %6.3f\n',nnz(sign(A*x)-co)/m)
-
-ReoveryShow(xo,x,[1000, 550, 400 200],1),pause(0.05)
-figure('Renderer', 'painters', 'Position', [1000, 200, 400 200])
-semilogy(1:out.iter,  out.OBJ,'b:','LineWidth',2), hold on
-xlabel('iteration'), ylabel('objective')
-grid on, legend('GPSP')
-
-
+ReoveryShow(xo,x,[1000 450 500 250])
