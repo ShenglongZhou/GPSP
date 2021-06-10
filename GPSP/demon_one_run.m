1826ef5a200f88fba3e29a8f20c6cbe6651b464e
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2021-06-10 18:44:41 +0100

    Update demon_one_run.m

--- GPSP/demon_one_run.m
+++ GPSP/demon_one_run.m
@@ -4,7 +4,7 @@ n          = 2000;
 m          = ceil(0.5*n);
 s          = ceil(0.01*n);  % sparsity level
 r          = 0.01;          % flipping ratio
-type       = 'Ind';         % 'Ind' or 'Cor' 
+type       = 'Ind';         % or 'Cor' 
 [A,c,co,xo]= random1bcs(type,m,n,s,r);
 
 k          = ceil(0.01*m);
@@ -15,3 +15,7 @@ fprintf('SNR:  %6.3f\n',-10*log10(norm(x-xo)^2));
 fprintf('HD:   %6.3f\n',nnz(sign(A*x)-c)/m)
 fprintf('HE:   %6.3f\n',nnz(sign(A*x)-co)/m)
 ReoveryShow(xo,x,[1000 450 500 250])
+
+
+
+
