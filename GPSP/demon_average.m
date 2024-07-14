ef5f1d85b3f293e080ee4f1fee7662d6e66b0a13
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2024-07-14 23:49:45 +0800

    Update demon_average.m

--- GPSP/demon_average.m
+++ GPSP/demon_average.m
@@ -9,11 +9,11 @@ k    = ceil(r*m);     % Upper bound of sign flips
 v    = 0.5;           % Correlation parameter
 
 Type = 'Ind';         % or 'Cor' 
-test = 's';           % change 'test' to see effect of GPSP to 
+test = 'v';           % change 'test' to see effect of GPSP to 
                       % factors {'s','m','r','v','n'}
 
 switch test
-  case 'm',   test0 = linspace(0.1,1.5,8);   
+  case 'm',   test0 = linspace(0.2,2,10);   
   case 's',   test0 = 2:10;  
   case 'r',   test0 = 0.02:0.02:0.2;  
   case 'v',   test0 = 0.1:0.1:0.9; Type = 'Cor';
@@ -21,7 +21,7 @@ switch test
 end
 
 f      = isequal(test,'n');  
-S      = 100*(1-f)+10*f;
+S      = 200*(1-f)+10*f;
 recd   = zeros(nnz(test0),4);
 for j  = 1:nnz(test0)
     switch test
@@ -36,25 +36,29 @@ for j  = 1:nnz(test0)
         % Generate data 
         [A,c,co,xo] = random1bcs(Type,m,n,s,r,0.1,v);
         out         = GPSP(A,c,s,k);      
-        recd(j,1)   = recd(j,1) - 20*log10(norm(xo-out.x));    
+        recd(j,1)   = recd(j,1) - 20*log10(norm(xo-out.x));      
         recd(j,2)   = recd(j,2) + nnz(sign(A*out.x)-c)/m;
-        recd(j,4)   = recd(j,4) + nnz(sign(A*out.x)-co)/m;
-        recd(j,3)   = recd(j,3) + out.time;
+        recd(j,3)   = recd(j,3) + nnz(sign(A*out.x)-co)/m;
+        recd(j,4)   = recd(j,4) + out.time;
     end
 end
 
 recd = recd/S; 
-ylab = {'SNR','HD','TIME','HE'};
-figure('Renderer', 'painters', 'Position', [900, 200, 450 350])
-xloc = [-0.05  0.01 -0.05  0.01];
-yloc = [ 0.01  0.01 -0.02 -0.02];
+
+close all;
+ylab = {'SNR','HD','HE','TIME'};
+figure('Renderer', 'painters', 'Position', [200, 50, 900 190])
+xloc = [ -0.09  -0.045  0.005  0.04];
 for j  = 1:4
-    sub  = subplot(2,2,j); 
+    sub  = subplot(1,4,j); 
     pos = get(sub, 'Position'); 
-    tmp  = recd(:,j);
-    plot(test0,tmp,'black.-','LineWidth',0.75), hold on,
-    grid on, xlabel(test), ylabel(ylab{j})  
-    axis([min(test0) max(test0) min(tmp)/1.1 max(tmp)*1.1]) 
-    set(sub, 'Position',pos+[xloc(j),yloc(j),0.06,0.05] )
+    tmp = recd(:,j);
+    if j < 4
+        plot(test0,tmp,'black.-','LineWidth',0.75), hold on,
+        axis([min(test0) max(test0) 0-2*(j==1) 0.35+33*(j==1)]); 
+    else
+        semilogy(test0,tmp,'black.-','LineWidth',0.75), hold on,
+    end
+    grid on, xlabel(test), ylabel(ylab{j})   
+    set(sub, 'Position',pos+[xloc(j),0,0.04,-0.01] )
 end   
- 
